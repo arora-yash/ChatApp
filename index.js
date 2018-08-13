53534f0041d83145f03bacd25f87c99d4e31b90b
Yash Kumar Arora <41463797+arora-yash@users.noreply.github.com>
2018-08-13 14:43:47 +0530

    Some Changes were made

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express') ();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8100;
 
 app.get('/',function(req,res) {
 	res.sendFile(__dirname + '/index.html');
@@ -9,10 +9,10 @@ app.get('/',function(req,res) {
 
 io.on('connection',function(socket) {
 	socket.on('chat message',function(msg) {
-		io.emit('chat message:', msg);
+		io.emit('chat message',msg);
 	});
 });
 
-http.listen(process.env.PORT||5000,function() {
-	console.log('listening to port 5000');
+http.listen(port, function(){
+	console.log('listening to port',port);
 });
